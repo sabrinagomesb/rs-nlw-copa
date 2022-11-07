0534e50672a54a0d91d7fba72b413ae86b01afa6
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2022-11-07 20:55:54 -0300

    acrescenta modificações aula 3

---
+++ main.js
@@ -0,0 +1,52 @@
+function createdGame(player1Img, player1Name, hour, player2Img, player2Name) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1Img}.svg" alt="Bandeira do ${player1Name}">
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2Img}.svg" alt="Bandeira da ${player2Name}">
+    </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+    <div class="card">
+      <h2>
+        ${date}
+        <span>
+          ${day}
+        </span>
+      </h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW">
+  </header>
+  <main id="cards">
+    ${createCard(
+      "24/11",
+      "quinta",
+      createdGame("switzerland", "Suíça", "07:00", "cameroon", "Camarões") +
+      createdGame("brazil", "Brasil", "16:00", "serbia", "Sérvia") 
+    )}
+    ${createCard(
+      "28/11",
+      "segunda",
+      createdGame("cameroon", "Camarões", "07:00", "serbia", "Sérvia") +
+      createdGame("brazil", "Brasil", "13:00", "switzerland", "Suíça")
+    )}
+    ${createCard(
+      "02/12",
+      "sexta",
+      createdGame("cameroon", "Camarões", "16:00", "brazil", "Brasil") +
+      createdGame("serbia", "Sérvia", "16:00", "switzerland", "Suíça")
+    )}
+  </main>
+
+`
