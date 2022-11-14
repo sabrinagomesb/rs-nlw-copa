fb5659cdb85c41564f8aa0b70cfa1de89e5862a3
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2022-11-14 18:12:58 -0300

    adiciona todos as informacoes dos jogos de todos os grupos e botao de selecionar grupo

--- main.js
+++ main.js
@@ -68,7 +68,139 @@ function CreateGroup({
     )
 }
 
-const groupGdata = {
+const groupAData = {
+  name: "A",
+  player1: "qatar",
+  player2: "ecuador",
+  player3: "senegal",
+  player4: "netherlands",
+}
+
+ document.querySelector("#cardA").innerHTML =
+   createCard(
+     "20/11",
+     "domingo",
+     createdGame("qatar", "13:00", "ecuador")
+   ) +
+   createCard(
+     "21/11",
+     "segunda",
+     createdGame("senegal", "13:00", "netherlands")
+   ) +
+   createCard(
+     "25/11",
+     "sexta",
+     createdGame("qatar", "10:00", "senegal") +
+       createdGame("netherlands", "13:00", "ecuador")
+   ) +
+   createCard(
+     "29/11",
+     "terça",
+     createdGame("netherlands", "12:00", "qatar") +
+       createdGame("ecuador", "12:00", "senegal")
+   )
+
+const groupBData = {
+  name: "B",
+  player1: "england",
+  player2: "iran",
+  player3: "united states",
+  player4: "wales",
+  dateA: "21/11",
+  dateB: "25/11",
+  dateC: "29/11",
+  dayA: "segunda",
+  dayB: "sexta",
+  dayC: "terça",
+  hourA1: "10:00",
+  hourA2: "16:00",
+  hourB1: "07:00",
+  hourB2: "16:00",
+  hourC1: "16:00",
+  hourC2: "16:00",
+}
+
+const groupCData = {
+  name: "C",
+  player1: "argentina",
+  player2: "saudi arabia",
+  player3: "mexico",
+  player4: "poland",
+  dateA: "22/11",
+  dateB: "26/11",
+  dateC: "30/11",
+  dayA: "terça",
+  dayB: "sábado",
+  dayC: "quarta",
+  hourA1: "07:00",
+  hourA2: "13:00",
+  hourB1: "10:00",
+  hourB2: "16:00",
+  hourC1: "16:00",
+  hourC2: "16:00",
+}
+
+const groupDData = {
+  name: "D",
+  player1: "france",
+  player2: "australia",
+  player3: "denmark",
+  player4: "tunisia",
+  dateA: "22/11",
+  dateB: "26/11",
+  dateC: "30/11",
+  dayA: "terça",
+  dayB: "sábado",
+  dayC: "quarta",
+  hourA1: "10:00",
+  hourA2: "16:00",
+  hourB1: "07:00",
+  hourB2: "13:00",
+  hourC1: "12:00",
+  hourC2: "12:00",
+}
+
+const groupEData = {
+  name: "E",
+  player1: "spain",
+  player2: "costa rica",
+  player3: "germany",
+  player4: "japan",
+  dateA: "23/11",
+  dateB: "27/11",
+  dateC: "01/12",
+  dayA: "quarta",
+  dayB: "domingo",
+  dayC: "quinta",
+  hourA1: "10:00",
+  hourA2: "13:00",
+  hourB1: "07:00",
+  hourB2: "16:00",
+  hourC1: "16:00",
+  hourC2: "16:00",
+}
+
+const groupFData = {
+  name: "F",
+  player1: "belgium",
+  player2: "canada",
+  player3: "morocco",
+  player4: "croatia",
+  dateA: "23/11",
+  dateB: "27/11",
+  dateC: "01/12",
+  dayA: "quarta",
+  dayB: "domingo",
+  dayC: "quinta",
+  hourA1: "07:00",
+  hourA2: "16:00",
+  hourB1: "10:00",
+  hourB2: "13:00",
+  hourC1: "12:00",
+  hourC2: "12:00",
+}
+
+const groupGData = {
   name: "G",
   player1: "switzerland",
   player2: "cameroon",
@@ -88,6 +220,31 @@ const groupGdata = {
   hourC2: "16:00",
 }
 
+const groupHData = {
+  name: "H",
+  player1: "portugal",
+  player2: "ghana",
+  player3: "uruguay",
+  player4: "south korea",
+  dateA: "24/11",
+  dateB: "28/11",
+  dateC: "02/12",
+  dayA: "quinta",
+  dayB: "segunda",
+  dayC: "sexta",
+  hourA1: "10:00",
+  hourA2: "13:00",
+  hourB1: "10:00",
+  hourB2: "16:00",
+  hourC1: "12:00",
+  hourC2: "12:00",
+}
 
-
-CreateGroup(groupGdata)
+// CreateGroup(groupAData)
+CreateGroup(groupBData)
+CreateGroup(groupCData)
+CreateGroup(groupDData)
+CreateGroup(groupEData)
+CreateGroup(groupFData)
+CreateGroup(groupGData)
+CreateGroup(groupHData)
